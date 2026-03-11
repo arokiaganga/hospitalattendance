0947dac228aeb4e1fe108500c65eb741b1e42072
Felic <arokiaganga@gmail.com>
2026-03-11 15:03:29 +0530

    render port fix

--- server.js
+++ server.js
@@ -215,7 +215,14 @@ app.post("/attendance-code", async (req, res) => {
 
 
 
-app.listen(5000, "0.0.0.0", () => {
+/**app.listen(5000, "0.0.0.0", () => {
   console.log("Server running on port 5000");
+});**/
+
+
+
+ const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
- 
